0181e4f366b72a4796557844b4e8e205937c985b
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-27 06:46:29 +0000

    server update

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.listen(2023, () => {
     console.log("listening on port 2023");
 })
 
+//signup
+app.get("/signup", (req,res) => {
+    res.sendFile("signup.html", {root: "./"})
+})
+
 //404 error
 app.get("/404", (req, res) => {
     res.sendFile("404.html", {root: "./"})
