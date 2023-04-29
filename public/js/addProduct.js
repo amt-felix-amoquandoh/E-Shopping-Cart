24ee8ebf1e845d387edb2bbab093b8d7df16a155
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-29 15:04:14 +0000

    product forms

--- public/js/addProduct.js
+++ public/js/addProduct.js
@@ -11,3 +11,18 @@ ratingsInput.map((star, index) => {
         }
     })
 })
+
+let user = JSON.parse(sessionStorage.user || null);
+
+window.onload = () => {
+    if(user == null){
+        location.replace("../../login.html")
+    }
+}
+
+let editables = [...document.querySelectorAll("*[contenteditable='true']")];
+
+editables.map((element) => {
+    let placeholder = element.getAttribute("data-placeholder");
+    element.innerHTML = placeholder;
+})
